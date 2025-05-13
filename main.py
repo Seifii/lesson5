69933e37a513a340ee12034370098bdf109a4569
Seifii <sofiapetrova893@gmail.com>
2025-05-13 16:57:49 +0300

    #3

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print()
+# class Human:
+#     height = 170
+#     satiety = 50
+# class Student(Human):
+#     safety = 0
+# class Worker(Human):
+#     satiety = 100
+class Grandparent:
+    height = 170
+    satiety = 100
+    age = 60
+class Parent(Grandparent):
+    age = 40
+class Child(Parent):
+    height = 50
+    def __init__(self):
+        print(self.height)
+        print(self.satiety)
+        print(self.age)
+
+
+nick = Child()
+
+nick = Student()
+ann = Worker()
+print(nick.satiety)
+print(ann.satiety)
